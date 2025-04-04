94176a300acc31637414fb759d3c9362ef5c668a
gk <kalomirisgiannis@gmail.com>
2025-04-04 12:06:11 +0300

    init commit

---
+++ src/main/java/com/example/match_reactive/MatchReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.match_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchReactiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/match_reactive/MatchReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.match_reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
